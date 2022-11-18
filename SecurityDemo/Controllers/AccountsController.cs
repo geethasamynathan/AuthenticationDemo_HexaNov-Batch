165f5077f2cd2e71612c016a2447f15a7d70a0ed
geethasamynathan <geethasamynathan2011@gmail.com>
2022-11-18 10:29:51 +0530

    Add project files.

---
+++ SecurityDemo/Controllers/AccountsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using SecurityDemo.Models;
+namespace SecurityDemo.Controllers
+{
+    public class AccountsController : Controller
+    {
+        // GET: Accounts
+       
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(User usermodel)
+        {
+            using (MVC_DBEntities entities = new MVC_DBEntities())
+            {
+                bool IsValidUser = entities.Users.Any(
+                    user => user.UserName.ToLower() == usermodel.UserName
+                    && user.UserPassword == usermodel.UserPassword);
+
+                if (IsValidUser)
+                {
+                    FormsAuthentication.SetAuthCookie(usermodel.UserName, false);
+                    return RedirectToAction("Index", "Employees");
+                }
+                ModelState.AddModelError("", "Invalid Username or Password");
+                return View();
+            }
+        }
+    }
+}
